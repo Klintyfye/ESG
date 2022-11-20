d61ebb935b26850b11df973f474554099c824869
saifaldeen15 <saifalkadahin.15@gmail.com>
2022-11-20 21:04:35 +0100

    .

---
+++ api.py
@@ -0,0 +1,80 @@
+import http.client # Required for communication with API
+import json # Required for JSON conversion
+from werkzeug.utils import secure_filename
+def autocomplete(extension_name):
+    """Get autocomplete suggestions from the chrome webstore.
+    Parameter: String
+    Return: Array
+    """
+    # conn = http.client.HTTPSConnection("chrome.google.com")
+    # headers = { 'cookie': "NID=511%3DLiws3dmziXl-QBqvTLHF-h8I-KY49YqO-vOsxEVAQ7llUOdXBj9RdDFK8MJslTvgezpvoE8ueo_x2dl4a3cC1cWne60X29sLzwWVJALTz88prWW0-H1pN0nikP3WtURYRftbfmvSsqvZxjXuEVnrU3sVDymPDyVaPlDykJQNTmI; CONSENT=PENDING%2B895" }
+    # conn.request("POST", "/webstore/search/autocomplete?pv=20210820&q=" + extension_name, "", headers)
+
+    # res = conn.getresponse()
+    # data = res.read()
+
+    # string = data.decode("utf-8").replace(")]}'\n\n", "", 1)
+    # alist = json.loads(string)[1][2]
+    # return alist
+
+
+def get_item(alist):
+    """Retrieves basic extension information.
+    Parameter: String (Extension name)
+    Return: Array (Link to extension, Extension ID, Full extension name, Image link, Description, Amount of reviews, Score our of 5)
+    """
+    # clist = []
+    # for i in alist:
+    blist = [[], [], [], []]
+    alist = secure_filename(alist)
+    conn = http.client.HTTPSConnection("chrome.google.com")
+    headers = { 'cookie': "NID=511%3Dcy1Y33x_e4r3px-iJ6uv1Mvh6gccjaOXC3x_USnO7gLk5JczW3vkTmtk97s_dG9fhU2oVKzI4rqkbTXSQe02VnxT9RXaLmTljAx8V4y0G9pAMoua1jZWBe7J_ovwwO-YsFyny6bVC6i9gF1iUQ3kZ7JKRQ7pv1YPu3ypjawopbMtRMXgJhQwwTAgS16NbAEwI_NvjAgW; CONSENT=PENDING%2B895" }
+    conn.request("POST", "/webstore/ajax/item?pv=20210820&count=112&searchTerm="+ alist,"", headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    string = data.decode("utf-8").replace(")]}'\n\n", "", 1)
+    alist = json.loads(string)
+    # print(alist)
+    if len(alist[1][1]) >4:
+        for i in range(4):
+            
+            # print('\n''####################################################')
+            blist[i].append(alist[1][1][i][37]) 
+            blist[i].append(alist[1][1][i][1]) 
+            blist[i].append(alist[1][1][i][4])
+            blist[i].append(alist[1][1][i][6]) 
+            blist[i].append(alist[1][1][i][22]) 
+            blist[i].append(alist[1][1][i][12])
+        # print(blist)
+    else:
+        for i in range(len(alist[1][1])):
+            
+            # print('\n''####################################################')
+            blist[i].append(alist[1][1][i][37]) 
+            blist[i].append(alist[1][1][i][1]) 
+            blist[i].append(alist[1][1][i][4])
+            blist[i].append(alist[1][1][i][6]) 
+            blist[i].append(alist[1][1][i][22]) 
+            blist[i].append(alist[1][1][i][12])
+        # print(blist)
+
+        # print('\n''####################################################')
+
+    return blist
+
+
+# # For testing
+# test_value = 'adblock'
+
+# autocomplete(test_value)
+
+# get_item(test_value)
+
+# ['adblock plus', 'adblock for youtube']
+
+# tlist = [['https://chrome.google.com/webstore/detail/adblock-%E2%80%94-best-ad-blocker/gighmmpiobklfepjocnamgkkbiglidom', 'AdBlock — den bästa annonsblockeraren', 'https://lh3.googleusercontent.com/3WssruYpy1oFSsMEQol5IZFGPYI7uYgwfekPl85NqKaUxu2bJsveNdO9oII2fYeBgznWbL-X5AtBidNa9ddFowwd7pM=w220-h140-e365-rj-sc0x00ffffff', 'Blockera reklam, pop-ups på YouTube, Facebook, Twitch och dina andra favoritsidor.', 296928, 4.520779448216403], ['https://chrome.google.com/webstore/detail/adblock-for-youtube/cmedhionkhpnakcndndgjdbohmhepckk', 'Adblock for Youtube™', 'https://lh3.googleusercontent.com/s461fSyCaZWFkKsCvs8jsZV-SW_szvxsJwKFN8CgVpNXNE4DBM_ZpGa7glQrVEDemSTkTQMx3KqA_DBEtkJHR-DXSA=w220-h140-e365-rj-sc0x00ffffff', 'Tar bort annonser från Youtube.', 260125, 4.396790004805382], ['https://chrome.google.com/webstore/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb', 'Adblock Plus - gratis annonsblockerare', 'https://lh3.googleusercontent.com/5KhDb_CYPuUIBdZgLu4AOFIRjerLSQdC9Jrbv5ReXPQudO7_RlcNHZZJDqNrWQpoN2-xMg20j2uASzVdBoI1sq0kECo=w220-h140-e365-rj-sc0x00ffffff', 'Blockera YouTube™-annonser, popup-fönster & bekämpa mot skadlig kod!', 181687, 4.455134379454776], ['https://chrome.google.com/webstore/detail/crystal-ad-block/lklmhefoneonjalpjcnhaidnodopinib', 'Crystal Ad block', 'https://lh3.googleusercontent.com/CI44xBkM86uYrI7vSKAGpyXvI8P_gZon17lLhdf6SwbkukAH_ihgHMucufqPl1mx0L2BWhMLz23n-NGjGBTY3nOtpA=w220-h140-e365-rj-sc0x00ffffff', 'Crystal ad block - Open source and transparent ad block software.', 30, 4.2]]
+
+# for i in tlist:
+#     print(i[0].split('/')[-1])
--- server2.py
+++ server2.py
@@ -1,15 +1,16 @@
 import os      # For File Manipulations like get paths, rename
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
-import json
 from fileinput import filename
-import requests
-import time
 from zipfile import ZipFile
+import retireJS 
+import virustotal 
+import api 
+import crx_downloader
 
 
 app=Flask(__name__)
-app.secret_key = "secret key" # for encrypting the session
+app.secret_key = "secret key" # for encrypting the sessions
 #It will allow below 16MB contents only, you can change it
 app.config['MAX_CONTENT_LENGTH'] = 250 * 1024 * 1024
 path = os.getcwd()
@@ -28,154 +29,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['crx'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-def virustotal( path):
-    path = path
-    url_for_large_file()
-    url_for_upload(path)
-    url_for_analysis_report(path)
-    url_for_analysis_report_from_hash(path)
-    if_file_queued(path)
-#path = "C:/Users/saifa/OneDrive/Dokument/DV1512/projekt_code/flask/uploadeZip.zip"
-# path = "/mnt/c/Users/saifa/OneDrive/Dokument/DV1512/projekt_code/flask/uploadeZip.zip"
-
-
-def url_for_large_file():
-
-    url_for_large_files = "https://www.virustotal.com/api/v3/files/upload_url"
-    refined_url = ""
-    cnt = 15
-    first_header = {
-        "accept": "application/json",
-        "x-apikey": "51bff97d51ac2996b0b64155402f55cc8960cdfbfcd3f5361eff4fd9f2dc65b2"
-    }
-    
-    first_second_response = requests.get(url_for_large_files, headers=first_header)
-    while (first_second_response.text[cnt] != '"'):
-        refined_url += first_second_response.text[cnt]
-        cnt += 1
-    time.sleep(3)
-    return refined_url
     
 
-def url_for_upload(path):
-    url_for_upload = url_for_large_file()
-    files = {"file": open(path, "rb")}
-    second_header = {
-        "accept": "application/json",
-        "x-apikey": "4926e23cc9acaa6110047cd35fa667a46e8acf4c889d5d7881e8859008df4e3c"
-    }
-
-    second_response = requests.post(url_for_upload, files=files, headers=second_header)
-
-    check = True
-    stringedRes = ""
-    stringresp = second_response.text
-
-    for i in range(len(stringresp)):
-        if stringresp[i] == "i" and stringresp[i+1] == "d":
-            i = i+5
-            while check:
-                if stringresp[i+1] == '"':
-                    check = False
-                    break
-                stringedRes = stringedRes + stringresp[i+1]
-                i = i+1
-    #print(url_for_upload)
-    time.sleep(5)
-    #print(stringedRes)
-    return stringedRes
-    
-
-def url_for_analysis_report(path):
-    rest_of_string = url_for_upload(path)
-    url_for_analysis_report = "https://www.virustotal.com/api/v3/analyses/"+rest_of_string
-    fourth_header = {
-        "accept": "application/json",
-        "x-apikey": "d16649ccb180832552aa0a0d6a91b57103da5fd1a6d979cda653401f301185e6"
-    }
-    third_response = requests.get(url_for_analysis_report, headers=fourth_header)
-    json_data = json.loads(third_response.text)
-    return json_data
-    
-def if_file_queued(path):
-    switch = True
-    while switch:
-        info = url_for_analysis_report(path)
-        if info["data"]["attributes"]["status"] == "completed":
-            switch = False
-            break
-        print("Process is in queue, please wait...")
-        time.sleep(5) 
-            
-        
-
-
-    filename = "virustotal_output.json"
-    sha256_value = info["meta"]["file_info"]["sha256"]
-    #print(sha256_value)
-    with open(filename, "w") as file_object:
-        json.dump(info, file_object)
-        
-
-    
-        
-    #time.sleep(3)
-    return sha256_value
-    
-    
-
-def url_for_analysis_report_from_hash(path):
-    sha256 = if_file_queued(path)
-    url_for_analysis_hash = "https://www.virustotal.com/api/v3/files/"+sha256
-    fourth_header = {
-        "accept": "application/json",
-        "x-apikey": "51bff97d51ac2996b0b64155402f55cc8960cdfbfcd3f5361eff4fd9f2dc65b2"
-    }
-
-    fourth_response= requests.get(url_for_analysis_hash, headers=fourth_header)
-    json_data1 = json.loads(fourth_response.text)
-    #print(json_data1)
-    filename = "virustotal_output.json"
-    with open(filename, "w") as file_object:
-       json.dump(json_data1, file_object)
-
-    
-def retireJS(path2):
-    path = path2
-    print("Scanning, please wait...")
-
-    os.system("retire --outputpath temp.json --outputformat json --path " + path)
-    print("Scan successful!")
-
-    print("\nCleaning, please wait...")
-
-    f = open("humanOutput.txt", "w")
-    with open("temp.json") as json_file:
-        file = json.load(json_file)
-        
-        # Print the file of dictionary
-        for object in file["data"]:
-            f.write("////////////////////////////\n")
-            f.write("Vulnerable file:"+ object["file"]+"\n")
-            for results in object["results"]:
-                f.write("\tComponent:"+ results["component"]+"\n")
-                for vulnerabilities in results["vulnerabilities"]:
-                    f.write("\t\tSummary:"+ vulnerabilities["identifiers"]["summary"]+"\n")
-                    f.write("\t\t\tCVE:"+ vulnerabilities["identifiers"]["CVE"][0]+"\n")
-                    f.write("\t\t\tSeverity:"+ vulnerabilities["severity"]+"\n")
-
-    f.close()
-    print("Clean successful!")
 
 @app.route('/')
 def upload_form():
     return render_template('upload.html')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def upload_file():
+
     if request.method == 'POST':
-        file = request.files['files[]']
+
+        file = request.files['crxfile']
+        if not file:
+            flash('No file uploaded!')
+            return redirect('/')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_CRX_FOLDER'], filename))
@@ -187,11 +57,24 @@ def upload_file():
             crx_unzip.extractall(
                 path = os.path.join(app.config['UPLOAD_FOLDER'], filename))
         path2 = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-      
-        virustotal(path)
-        retireJS(path2)
-        flash('Folder successfully uploaded')
+        virustotal.virustotal(path)
+        retireJS.retireJS(path2)
+        flash('File successfully uploaded')
         return redirect('/')
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        extension_name = request.form.get('search')
+        if not extension_name:
+            flash('No input')
+            return redirect('/')
+
+        extension_ifo_list = api.get_item(extension_name)
+        for i in range(len(extension_ifo_list)):
+            name = extension_ifo_list[i][0].split('/')[-1]
+            extension_ifo_list[i].append(name)
+        print('\n')
+        return render_template('upload.html', content = extension_ifo_list )
 
 # @app.route('/analyze', methods=['POST', 'GET'])
 # def analys():
@@ -212,21 +95,6 @@ def upload_file():
 #         virustotal.virustotal(path)
 #         retireJS.retireJS(path2)
 #         flash('File successfully uploaded')
-
-# crx_downloader.download_crx(extension_ifo_list[0][0])
-        # name = extension_ifo_list[0].split('/')
-        # # print(name[5])
-        # path = os.path.join(app.config['UPLOAD_CRX_FOLDER'], name[5]+'.crx')
-        # with ZipFile(path) as crx_unzip:
-        # # Extracting all the members of the zip 
-        # # into a specific location.
-        #     crx_unzip.extractall(
-        #         path = os.path.join(app.config['UPLOAD_FOLDER'], name[5]+'.crx'))
-        # path2 = os.path.join(app.config['UPLOAD_FOLDER'], name[5]+'.crx')
-        # # print(path2)
-        # virustotal.virustotal(path)
-        # retireJS.retireJS(path2)
-        # flash('File successfully uploaded')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5000,debug=True,threaded=True)
