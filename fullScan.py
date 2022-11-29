e3e90b22b7e3db62b36577c7adc0cdfb70a81e18
feai19 <116172615+feai19@users.noreply.github.com>
2022-11-29 16:44:17 +0100

    Added virustTotal

--- fullScan.py
+++ fullScan.py
@@ -3,6 +3,7 @@ from typing import Literal
 import mongoAPI
 import resultCompile
 import runRetire
+import virusTotal
 
 def scan(crxDir: str, cwsId: str) -> Literal[-1,1]:
     """Takes path of crx and its cwsId and scans, compiles results, and uploads to database
@@ -15,8 +16,10 @@ def scan(crxDir: str, cwsId: str) -> Literal[-1,1]:
         Literal[0,1]: returns 1 on databse upload success or -1 on databse upload failure.
     """
 
-    #how ever tf you run the virus total idk how to make it work nicely.
-    vtDir = "vtExample.json"
+    print("Börja vt")
+    #how ever tf you run the virus total api idk how to make it work nicely.
+    vtDir = "vtResult.json"
+    virusTotal.virustotal(crxDir)
 
 
     #Name of folder which the crx extracts into
@@ -25,10 +28,12 @@ def scan(crxDir: str, cwsId: str) -> Literal[-1,1]:
     with ZipFile(crxDir, 'r') as f:
         f.extractall(extractedDir)
 
+    print("Börja retire")
     #Run retireJS scan on folder and create result json "retireResult.json"
     runRetire.runRetire(extractedDir)
     jsDir = "retireResult.json"
 
+    print("Börja compile")
     #Compiles results from retireJS and virusTotal
     object = resultCompile.compileResult(jsDir, vtDir, cwsId)
 
@@ -38,6 +43,9 @@ def scan(crxDir: str, cwsId: str) -> Literal[-1,1]:
 
 
 if __name__ == '__main__':
-    crxDir = "crx.crx"
-    cwsId = "123"
+    #crxDir = "crx.crx"
+    #cwsId = "123"
+    crxDir = input("crxDir: ")
+    cwsId = input("cwsId: ")
     scan(crxDir, cwsId)
+    print("Done")
