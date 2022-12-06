c372624ebc120b9083ed8e99d09d1aec8a95fea6
feai19 <116172615+feai19@users.noreply.github.com>
2022-12-06 21:39:58 +0100

    Updated history graph to include retire threat levels

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ def upload_file():
     scans the file and the unzipd folder in virustotal and retireJS
     returen: INTE KLAR ÄN
     """
-
+    
     #Checks request method. If not post, don't do anything
     if request.method == 'POST':
         #Read file by filename from html
@@ -80,7 +80,7 @@ def upload_file():
             flash('Only crx files')
             return redirect('/')
 
-        #If everything was successful move on to loading.html
+        #If everything was successful move on to loading.html 
         flash('File successfully uploaded')
         return render_template('loading.html')
 
@@ -94,22 +94,22 @@ def results():
     with open(path,"rb") as f:
         bytes = f.read() # read entire file as bytes
         readable_hash = hashlib.sha256(bytes).hexdigest()
-
+    
     #Check if extension already exists in database
     exist = mongo_API.getByHash(readable_hash)
-
+    
     #if extension is NOT in database
     if exist == None:
         #Get extension id
         extension_id= path.split('/')[-1]
 
-        #Extension id NOT ending in "crx" signifies CWS
+        #Extension id NOT ending in "crx" signifies CWS 
         if extension_id.split('.')[-1] != 'crx':
             extension_info = CWS_API.get_item(extension_id)
 
             #Gathers metadata of extension
             meta = {"cwsId":extension_id, "name": extension_info[0][1]}
-
+            
             #Scans crx
             scan.scan(path, meta)
 
@@ -119,7 +119,7 @@ def results():
 
             #Renders result
             return render_template("results.html", extension_info = extension_info[0] ,result = result,test = test, test2 = history_img )
-
+        
         #Extension id ending in "crx" signifies local upload
         else:
             #Gathers metadata of extension
@@ -133,10 +133,10 @@ def results():
 
             #Renders result
             return render_template("results.html", result = result)
-
+    
     #if extension is already in database
     else:
-
+        
         #Get extension id
         extension_id= path.split('/')[-1]
         extension_info = CWS_API.get_item(extension_id)
@@ -146,24 +146,24 @@ def results():
         history_img = history(extension_id)
 
         print('########################')
-
+        
         return render_template("results.html", result = exist, extension_info = extension_info,test = test, test2 = history_img)
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
     """ Takes a search term and returns a list of search results to the Web UI"""
-
+    
     #Checks request method. If not post, don't do anything
     if request.method == 'POST':
         extension_name = request.form.get('search')
-
+        
         #Cancel if no extension name
         if not extension_name:
             flash('No input')
             return redirect('/')
-
+        
         extension_info_list = CWS_API.get_item(extension_name)
-
+        
         #If item extension does not exist cancel
         if extension_info_list == []:
             flash('No extensions found')
@@ -202,7 +202,7 @@ def pie(filename):
         bytes = f.read() # read entire file as bytes
         readable_hash = hashlib.sha256(bytes).hexdigest();
 
-    print(readable_hash)
+    #print(readable_hash)
     result = mongo_API.getByHash(readable_hash)
 
     labels = []
@@ -247,27 +247,28 @@ def history(id):
     for object in result:
         #adds just the date as time isn't that important and cuts the first two numbers of the year
         dates.append(str(object["meta"]["date"]).split()[0][2:])
-        risks.append(int(object["retireSeverity"]))
+        risks.append(object["retireSeverity"])
+
+
+    #zip the lists to a touple list
+    zipped = zip(risks,dates)
+    zipped = list(zipped)
 
     #Sorts risks dependant on the order of dates
-    #zip the lists to a touple list
-    ziped = zip(dates,risks)
-    #Sort
-    sort = sorted(ziped)
-    temp = []
-    #add risks to empty list in order of after they've been sorted by dates
-    for i in sort:
-       temp.append(i[1])
-    #overwrite risks with sorted version
-    risks = temp
+    sort = sorted(zipped, key = lambda x: x[1])
 
-    #sort dates
-    dates.sort()
+    #reset risks and dates
+    risks = []
+    dates = []
+    #Re-add them in order
+    for item in sort:
+        risks.append(item[0])
+        dates.append(item[1])
 
     fig, ax = Figure.subplots()
     #define chart
 
-    #
+    #Setup value lists for risks
     none = []
     low = []
     medium = []
@@ -280,6 +281,7 @@ def history(id):
         high.append(x["high"])
         critical.append(x["critical"])
 
+    #Plot every function for the risks after dates.
     ax.plot(dates, none, label="none")
     ax.plot(dates, low, label="low")
     ax.plot(dates, medium, label="medium")
